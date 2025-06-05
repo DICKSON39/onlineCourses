5f79234d34da400b7a12f5875e9965ac26f72048
Dickson Ndumia <dicksonndumia19@gmail.com>
2025-06-05 10:53:15 +0300

    first commit

--- src/app/pages/my-paid-classes/my-paid-classes.component.ts
+++ src/app/pages/my-paid-classes/my-paid-classes.component.ts
@@ -1,37 +1,44 @@
-import { Component,OnInit } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ClassService} from '../../services/class.service';
-import {ReactiveFormsModule} from '@angular/forms';
-
-import {RouterLink} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClassService } from '../../services/class.service';
 
 @Component({
-  selector: 'app-my-paid-classes',
-  imports: [CommonModule,ReactiveFormsModule,RouterLink],
-  templateUrl: './my-paid-classes.component.html',
-  styleUrl: './my-paid-classes.component.css'
+  selector: 'app-class-player',
+  templateUrl: './class-player.component.html',
+  styleUrls: ['./class-player.component.css'],
 })
-export class MyPaidClassesComponent  implements OnInit {
-  classes: any[] = [];
- isLoading = true;
-  skeletonArray = Array(3);
-  constructor( private  classService : ClassService ) {
+export class ClassPlayerComponent implements OnInit {
+  classData: any;
+  selectedVideo: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private classService: ClassService
+  ) {}
+
+  ngOnInit() {
+    // Get the class id from route params
+    const classId = this.route.snapshot.paramMap.get('id');
+    if (classId) {
+      this.loadClassById(classId);
+    }
   }
 
-    ngOnInit(): void {
-
+  loadClassById(id: string) {
     this.classService.getMyPaidClasses().subscribe({
-      next: (res)=> {
-        this.classes = res.classes;
-        this.isLoading = false;
+      next: (res) => {
+        this.classData = res; // adjust depending on your API response
+        if (this.classData.videos?.length > 0) {
+          this.selectedVideo = this.classData.videos[0]; // auto-play first video
+        }
       },
-      error: (err)=> {
-        console.error('Error', err)
-        this.isLoading = false;
-      }
-    })
+      error: (err) => {
+        console.error('Error loading class', err);
+      },
+    });
+  }
 
-
-    }
-
+  playVideo(video: any) {
+    this.selectedVideo = video;
+  }
 }
